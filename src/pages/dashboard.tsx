168874c95908894660e105daeb372e7e88ddf568
Mohamed Ramadan <mohamedahmedramadan154@gmail.com>
2026-05-02 15:14:12 +0300

    feat: enhance Dashboard with new card components and styles for better data visualization

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,24 +1,62 @@
+import {
+  CircleAlert,
+  DollarSign,
+  ShoppingCart,
+  TrendingUp,
+  Truck,
+  Users,
+} from 'lucide-react';
 import { useTranslation } from 'react-i18next';
 import DashboardCard from '@/components/dashboard-card.tsx';
-import Soon from '@/components/soon.tsx';
 
 function Dashboard() {
   const { t } = useTranslation();
 
   return (
-    <Soon>
-      <div>
-        <div className="flex justify-between items-center mb-4">
-          <h2 className="text-xl font-semibold pb-2">{t('Dashboard')}</h2>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-          <DashboardCard title={t('Total Revenue')} value={12345} />
-          <DashboardCard title={t('Total Customers')} value={678} />
-          <DashboardCard title={t('Total Orders')} value={90} />
-          <DashboardCard title={t('Total Products')} value={45} />
-        </div>
+    <div>
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-xl font-semibold pb-2">{t('Dashboard')}</h2>
       </div>
-    </Soon>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <DashboardCard
+          title={t('Total sales')}
+          value={12345}
+          color="success"
+          icon={<DollarSign />}
+        />
+        <DashboardCard
+          title={t('Total Purchases')}
+          value={678}
+          color="info"
+          icon={<ShoppingCart />}
+        />
+        <DashboardCard
+          title={t('Profit')}
+          value={90}
+          color="error"
+          icon={<TrendingUp />}
+        />
+        <DashboardCard
+          title={t('Due from customers')}
+          value={45}
+          color="warning"
+          icon={<Users />}
+        />
+        <DashboardCard
+          title={t('Due to Providers')}
+          value={45}
+          color="warning"
+          icon={<Truck />}
+        />
+        <DashboardCard
+          title={t('Overdue payments')}
+          value={45}
+          color="error"
+          count={6}
+          icon={<CircleAlert />}
+        />
+      </div>
+    </div>
   );
 }
 
